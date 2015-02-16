ebcc2170a493457e4df0a5c30ac326f941fa8a0c
erinspace <erin.braswell@gmail.com>
2015-02-16 14:42:40 -0500

    added very basic check for duplicate provider entries

--- provider_registration/views.py
+++ provider_registration/views.py
@@ -1,12 +1,12 @@
 from django.utils import timezone
-
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import get_object_or_404, render
 
 from provider_registration.models import RegistrationInfo
 
 
 def index(request):
-    latest_provider_list = RegistrationInfo.objects.order_by('registration_date')[:5]
+    latest_provider_list = RegistrationInfo.objects.order_by('registration_date')
 
     context = {'latest_provider_list': latest_provider_list}
 
@@ -22,14 +22,18 @@ def register_provider(request):
     if request.method == 'POST':
         form_data = request.POST
 
-        RegistrationInfo(
-            provider_name=form_data['provider_name'],
-            base_url=form_data['base_url'],
-            property_list=form_data['property_list'],
-            approved_sets=form_data['approved_sets'],
-            registration_date=timezone.now()
-        ).save()
+        try:
+            RegistrationInfo.objects.get(provider_name=form_data['provider_name'])
+            return render(request, 'provider_registration/already_exists.html', {'provider': form_data['provider_name']})
+        except ObjectDoesNotExist:
+            RegistrationInfo(
+                provider_name=form_data['provider_name'],
+                base_url=form_data['base_url'],
+                property_list=form_data['property_list'],
+                approved_sets=form_data['approved_sets'],
+                registration_date=timezone.now()
+            ).save()
 
-        return render(request, 'provider_registration/confirmation.html', {'provider': form_data['provider_name']})
+            return render(request, 'provider_registration/confirmation.html', {'provider': form_data['provider_name']})
     else:
         return render(request, 'provider_registration/self_registration_form.html')
