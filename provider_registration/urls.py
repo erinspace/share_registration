9e315130b486b4fa55332ac270ef8ab8caa7d602
erinspace <erin.braswell@gmail.com>
2015-02-16 11:20:40 -0500

    update detail page to just display provider name

--- provider_registration/urls.py
+++ provider_registration/urls.py
@@ -5,5 +5,5 @@ from provider_registration import views
 urlpatterns = patterns(
     '',
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<provider_name>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<provider_name>.*)/$', views.detail, name='detail'),
 )
