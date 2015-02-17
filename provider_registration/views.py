0bfb1199e4f28ab1f2efc27947ca8f683fbee1e7
erinspace <erin.braswell@gmail.com>
2015-02-17 15:47:39 -0500

    now has 2 forms that pass data on incorrectly, about to change everything

--- provider_registration/views.py
+++ provider_registration/views.py
@@ -34,30 +34,35 @@ def detail(request, provider_name):
 
 
 def get_provider_info(request):
+    """ Makes 2 requests to the provided base URL:
+            1 for the sets available
+            1 for the list of properties
+        The sets available are added as multiple selections for the next form,
+        the properties are pre-loaded into the properties field.
+    """
     if request.method == 'POST':
         form_data = InitialProviderForm(request.POST)
         form_data.is_valid()  # TODO - fix this
         form_data = form_data.clean()
 
+        # request 1 for the setSpecs available
         set_url = form_data['base_url'] + '?verb=ListSets'
         set_data_request = requests.get(set_url)
         all_content = etree.XML(set_data_request.content)
         set_names = all_content.xpath('//oai_dc:setSpec/node()', namespaces=NAMESPACES)
         set_names = [name.replace('publication:', '') for name in set_names]
 
-        # request records 10 days back just in case
+        # request 2 for records 10 days back just in case
         start_date = str(date.today() - timedelta(10))
         prop_url = form_data['base_url'] + '?verb=ListRecords&metadataPrefix=oai_dc&from={}T00:00:00Z'.format(start_date)
         prop_data_request = requests.get(prop_url)
         all_prop_content = etree.XML(prop_data_request.content)
-
-        # this crazy thing is to get a list of properties from the first record
         pre_names = all_prop_content.xpath('//ns0:metadata', namespaces=NAMESPACES)[0].getchildren()[0].getchildren()
-
         all_names = [name.tag.replace('{' + NAMESPACES['dc'] + '}', '') for name in pre_names]
         property_names = list(set([name for name in all_names if name not in BASE_SCHEMA]))
 
-        return (set_names, property_names)
+        return {'set_names': set_names, 'property_names': property_names}
+
     else:
         form = InitialProviderForm()
         return render(
@@ -92,11 +97,11 @@ def register_provider(request):
                 {'provider': form_data['provider_name']}
             )
     else:
+        pre_populated_data = get_provider_info(request)
+        import ipdb; ipdb.set_trace()
         form = ProviderForm()
         return render(
             request,
             'provider_registration/self_registration_form.html',
             {'form': form}
         )
-
-
