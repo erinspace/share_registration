256d18d873b4ab53d758b4c5628428e172952e97
erinspace <erin.braswell@gmail.com>
2015-03-09 15:28:23 -0400

    add metada qustions to models

--- provider_registration/models.py
+++ provider_registration/models.py
@@ -3,11 +3,28 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+YES_NO_CHOICES = (
+    ('Y', 'yes'),
+    ('N', 'no')
+)
+
 
 class RegistrationInfo(models.Model):
 
+    # Basic Information
     provider_name = models.CharField(max_length=100, primary_key=True)
     base_url = models.CharField(max_length=100)
+    description = models.TextField()
+
+    # Terms of Service and Metadata Permissions Questions
+    meta_tos = models.CharField(max_length=1, choices=YES_NO_CHOICES)
+    meta_privacy = models.CharField(max_length=1, choices=YES_NO_CHOICES)
+    meta_sharing_tos = models.CharField(max_length=1, choices=YES_NO_CHOICES)
+    meta_license = models.CharField(max_length=100)
+    meta_license_extended = models.CharField(max_length=1, choices=YES_NO_CHOICES)
+    meta_future_license = models.CharField(max_length=1, choices=YES_NO_CHOICES)
+
+    # # Harvester Information
     property_list = models.TextField()
     approved_sets = models.TextField()
     registration_date = models.DateTimeField('date registered')
