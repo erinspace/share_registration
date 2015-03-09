d8b2a915f040acf0c723873f893902ba6c0bd3d8
erinspace <erin.braswell@gmail.com>
2015-03-09 15:28:54 -0400

    Add urls to static files

--- provider_registration/urls.py
+++ provider_registration/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from provider_registration import views
 
@@ -9,3 +10,5 @@ urlpatterns = patterns(
     url('self_register', views.register_provider, name='self_register'),
     url('pre_register', views.get_provider_info, name='pre_register'),
 )
+
+urlpatterns += staticfiles_urlpatterns()
