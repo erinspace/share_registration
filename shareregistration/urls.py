c276bdd35160bdf65927640075cd7d9dfe83125a
erinspace <erin.braswell@gmail.com>
2015-03-10 20:09:34 -0400

    add push urls to main urls

--- shareregistration/urls.py
+++ shareregistration/urls.py
@@ -5,5 +5,6 @@ urlpatterns = patterns(
     '',
     url(r'^provider_registration/', include('provider_registration.urls', namespace="provider_registration")),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^', include('push_endpoint.urls')),
 )
