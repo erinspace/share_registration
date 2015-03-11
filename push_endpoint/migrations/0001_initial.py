375548b75706a4949e01345dcdb68c4086d7a9fa
erinspace <erin.braswell@gmail.com>
2015-03-11 11:14:16 -0400

    update database for users

--- push_endpoint/migrations/0001_initial.py
+++ push_endpoint/migrations/0001_initial.py
@@ -2,11 +2,13 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
@@ -14,7 +16,16 @@ class Migration(migrations.Migration):
             name='PushedData',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('data', models.TextField()),
+                ('description', models.TextField()),
+                ('contributors', models.TextField()),
+                ('tags', models.TextField()),
+                ('source', models.TextField()),
+                ('title', models.TextField()),
+                ('dateUpdated', models.TextField()),
+                ('url', models.TextField()),
+                ('serviceID', models.TextField()),
+                ('doi', models.TextField()),
+                ('owner', models.ForeignKey(related_name='push_endpoint', to=settings.AUTH_USER_MODEL)),
             ],
             options={
             },
--- push_endpoint/models.py
+++ push_endpoint/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 
 class PushedData(models.Model):
-    description = models.TextField()  # TODO: make this a json field or seperate for each in normed data?
-    contributors = models.TextField()  # TODO: make this a json field or seperate for each in normed data?
-    tags = models.TextField()  # TODO: make this a json field or seperate for each in normed data?
-    source = models.TextField()  # TODO: make this a json field or seperate for each in normed data?
-    title = models.TextField()  # TODO: make this a json field or seperate for each in normed data?
-    dateUpdated = models.TextField()  # TODO: make this a json field or seperate for each in normed data?
-    url = models.TextField()  # TODO: make this a json field or seperate for each in normed data?
-    serviceID = models.TextField()  # TODO: make this a json field or seperate for each in normed data?
-    doi = models.TextField()  # TODO: make this a json field or seperate for each in normed data?
+    owner = models.ForeignKey('auth.User', related_name='push_endpoint')
+    description = models.TextField()
+    contributors = models.TextField()
+    tags = models.TextField()
+    source = models.TextField()
+    title = models.TextField()
+    dateUpdated = models.TextField()
+    url = models.TextField()
+    serviceID = models.TextField()
+    doi = models.TextField()
