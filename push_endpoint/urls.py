75126a7b8afe7aacbb43e2c1defea8b58e8efc21
erinspace <erin.braswell@gmail.com>
2015-03-11 14:31:57 -0400

    fix hyperlinked data urls

--- push_endpoint/urls.py
+++ push_endpoint/urls.py
@@ -4,9 +4,9 @@ from push_endpoint import views
 
 urlpatterns = [
     url(r'^pushed_data/$', views.DataList.as_view()),
-    url(r'^pushed_data/(?P<pk>[0-9]+)/$', views.DataDetail.as_view()),
+    url(r'^pushed_data/(?P<pk>[0-9]+)/$', views.DataDetail.as_view(), name='data-detail'),
     url(r'^users/$', views.UserList.as_view()),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view())
+    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='user-detail')
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
