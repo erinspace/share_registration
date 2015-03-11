f02ec81e25fe1526bfd2fc5f617a6a0cca6ba9f4
erinspace <erin.braswell@gmail.com>
2015-03-10 21:39:54 -0400

    update todo

--- push_endpoint/models.py
+++ push_endpoint/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 
 class PushedData(models.Model):
-    data = models.TextField()  # TODO: make this a json field?
+    data = models.TextField()  # TODO: make this a json field or seperate for each in normed data?
