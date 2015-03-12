fea0e41fc96aeb12e52e1ad7e963e1f10701ce2c
erinspace <erin.braswell@gmail.com>
2015-03-11 23:52:22 -0400

    Add dateupdated as order_by option

--- push_endpoint/views.py
+++ push_endpoint/views.py
@@ -20,6 +20,8 @@ class DataList(generics.ListCreateAPIView):
     serializer_class = PushedDataSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
+    ordering_fields = ('dateUpdated')
+
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
