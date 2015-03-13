c274ab0156aa3ecd3d853fcdaee35d0fcbc6014f
erinspace <erin.braswell@gmail.com>
2015-03-13 13:06:42 -0400

    triangles

--- provider_registration/models.py
+++ provider_registration/models.py
@@ -7,17 +7,17 @@ from django.utils import timezone
 class RegistrationInfo(models.Model):
 
     # Basic Information
-    provider_short_name = models.CharField(max_length=50, primary_key=True)
-    provider_long_name = models.CharField(max_length=100)
     base_url = models.URLField()
     description = models.TextField()
     oai_provider = models.BooleanField(default=False)
+    provider_long_name = models.CharField(max_length=100)
+    provider_short_name = models.CharField(max_length=50, primary_key=True)
 
     # Terms of Service and Metadata Permissions Questions
     meta_tos = models.BooleanField(default=False)
+    meta_license = models.CharField(max_length=100)
     meta_privacy = models.BooleanField(default=False)
     meta_sharing_tos = models.BooleanField(default=False)
-    meta_license = models.CharField(max_length=100)
     meta_license_extended = models.BooleanField(default=False)
     meta_future_license = models.BooleanField(default=False)
 
