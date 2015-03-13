dab1f912451a63cc2d194f1f49ee6c62d8bbc641
erinspace <erin.braswell@gmail.com>
2015-03-13 19:25:54 -0400

    test base

--- provider_registration/tests.py
+++ provider_registration/tests.py
@@ -1,3 +1,18 @@
+import datetime
+
+from django.utils import timezone
 from django.test import TestCase
 
-# Create your tests here.
+from provider_registration.models import RegistrationInfo
+
+
+class RegistrationMethodTests(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() should return False for questions whose
+        pub_date is in the future
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = RegistrationInfo(registration_date=time)
+        self.assertEqual(future_question.was_registered_recently(), False)
