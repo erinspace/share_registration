71d4bb792b86bcab9e7a24a0ccad42dff1866b87
erinspace <erin.braswell@gmail.com>
2015-03-17 15:48:57 -0400

    Remove outer if statement because form is never reached outside of post

--- provider_registration/views.py
+++ provider_registration/views.py
@@ -90,79 +90,78 @@ def register_provider(request):
     """ Function to register a provider. This does all the work for
     registration, calling out to other functions for processing
     """
-    if request.method == 'POST':
-        # If there's no prop list, it's a first request or non-oai request
-        if not request.POST.get('property_list'):
-            name = request.POST['provider_long_name']
-            base_url = request.POST['base_url']
-            # if it's a first request and not an oai request, render the other provider form
-            if not request.POST.get('oai_provider'):
-                save_other_info(name, base_url)
-                form = OtherProviderForm(
-                    {
-                        'provider_long_name': name,
-                        'base_url': base_url,
-                        'property_list': 'enter properties here'
-                    }
-                )
-                if form.is_valid():
-                    return render(
-                        request,
-                        'provider_registration/other_registration_form.html',
-                        {'form': form}
-                    )
-                else:
-                    print('NOT VALID.')
-                    form = InitialProviderForm()
-                    return render(
-                        request,
-                        'provider_registration/initial_registration_form.html',
-                        {'form': form}
-                    )
-            else:
-                # If it's made it this far, request is an OAI provider
-                logger.info("About to save {} in OAI format".format(name))
-                save_oai_info(name, base_url)
-                pre_saved_data = RegistrationInfo.objects.get(provider_long_name=name)
-
-                approved_set_list = pre_saved_data.approved_sets.split(',')
-                approved_set_list = [item.replace('[', '').replace(']', '') for item in approved_set_list]
-                approved_set_set = set([(item, item) for item in approved_set_list])
-                # render an OAI form with the request data filled in
-                form = OAIProviderForm(
-                    {
-                        'provider_long_name': name,
-                        'base_url': base_url,
-                        'property_list': pre_saved_data.property_list
-                    },
-                    choices=approved_set_set
-                )
+    # If there's no prop list, it's a first request or non-oai request
+    if not request.POST.get('property_list'):
+        name = request.POST['provider_long_name']
+        base_url = request.POST['base_url']
+        # if it's a first request and not an oai request, render the other provider form
+        if not request.POST.get('oai_provider'):
+            save_other_info(name, base_url)
+            form = OtherProviderForm(
+                {
+                    'provider_long_name': name,
+                    'base_url': base_url,
+                    'property_list': 'enter properties here'
+                }
+            )
+            if form.is_valid():
                 return render(
                     request,
-                    'provider_registration/oai_registration_form.html',
+                    'provider_registration/other_registration_form.html',
+                    {'form': form}
+                )
+            else:
+                print('NOT VALID.')
+                form = InitialProviderForm()
+                return render(
+                    request,
+                    'provider_registration/initial_registration_form.html',
                     {'form': form}
                 )
         else:
-            # If there's no approved sets yet, render the pre-OAI provider form
-            if request.POST.get('approved_sets', False):
-                choices = {(item, item) for item in request.POST['approved_sets']}
-                form_data = OAIProviderForm(request.POST, choices=choices)
+            # If it's made it this far, request is an OAI provider
+            logger.info("About to save {} in OAI format".format(name))
+            save_oai_info(name, base_url)
+            pre_saved_data = RegistrationInfo.objects.get(provider_long_name=name)
 
-                object_to_update = RegistrationInfo.objects.get(provider_long_name=form_data['provider_long_name'].value())
-
-                object_to_update.property_list = form_data['property_list'].value()
-                object_to_update.approved_sets = str(form_data['approved_sets'].value())
-
-                object_to_update.save()
-            else:
-                form_data = OtherProviderForm(request.POST)
-                object_to_update = RegistrationInfo.objects.get(provider_long_name=form_data['provider_long_name'].value())
-                object_to_update.property_list = form_data['property_list'].value()
-
-                object_to_update.save()
-
+            approved_set_list = pre_saved_data.approved_sets.split(',')
+            approved_set_list = [item.replace('[', '').replace(']', '') for item in approved_set_list]
+            approved_set_set = set([(item, item) for item in approved_set_list])
+            # render an OAI form with the request data filled in
+            form = OAIProviderForm(
+                {
+                    'provider_long_name': name,
+                    'base_url': base_url,
+                    'property_list': pre_saved_data.property_list
+                },
+                choices=approved_set_set
+            )
             return render(
                 request,
-                'provider_registration/confirmation.html',
-                {'provider': form_data['provider_long_name'].value()}
+                'provider_registration/oai_registration_form.html',
+                {'form': form}
             )
+    else:
+        # If there's no approved sets yet, render the pre-OAI provider form
+        if request.POST.get('approved_sets', False):
+            choices = {(item, item) for item in request.POST['approved_sets']}
+            form_data = OAIProviderForm(request.POST, choices=choices)
+
+            object_to_update = RegistrationInfo.objects.get(provider_long_name=form_data['provider_long_name'].value())
+
+            object_to_update.property_list = form_data['property_list'].value()
+            object_to_update.approved_sets = str(form_data['approved_sets'].value())
+
+            object_to_update.save()
+        else:
+            form_data = OtherProviderForm(request.POST)
+            object_to_update = RegistrationInfo.objects.get(provider_long_name=form_data['provider_long_name'].value())
+            object_to_update.property_list = form_data['property_list'].value()
+
+            object_to_update.save()
+
+        return render(
+            request,
+            'provider_registration/confirmation.html',
+            {'provider': form_data['provider_long_name'].value()}
+        )
