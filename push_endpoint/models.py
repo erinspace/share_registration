f70d736341571ae9ff6ac11a8a558024b6f4b0fd
erinspace <erin.braswell@gmail.com>
2015-03-18 20:50:17 -0400

    Add ability for description to be blank

--- push_endpoint/models.py
+++ push_endpoint/models.py
@@ -7,7 +7,7 @@ class PushedData(models.Model):
     tags = models.TextField(blank=True)
     title = models.TextField()
     serviceID = models.TextField()
-    description = models.TextField()
+    description = models.TextField(blank=True)
     contributors = models.TextField()
     dateUpdated = models.DateField(auto_now_add=True)
     source = models.ForeignKey('auth.User', related_name='data')
