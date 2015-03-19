87747aa20cd1ec826178a4503eb7f39ecf4886cf
erinspace <erin.braswell@gmail.com>
2015-03-18 20:13:59 -0400

    Add tests for valid submission and also DOIs

--- push_endpoint/tests.py
+++ push_endpoint/tests.py
@@ -1,3 +1,70 @@
-from django.test import TestCase
+import copy
+import json
 
-# Create your tests here.
+from django.test import TestCase
+from push_endpoint.views import DataList
+from django.contrib.auth.models import AnonymousUser, User
+from rest_framework.test import APIRequestFactory
+from rest_framework.test import force_authenticate
+
+
+VALID_POST = {
+    "description": "Ducks, their calls, and their habbits.",
+    "contributors": "Shawn Michaels",
+    "tags": "ducks, hunting",
+    "source": "devon",
+    "title": "All About Ducks",
+    "url": "http://dudley.net",
+    "serviceID": "DuckID11",
+    "doi": "sdf"
+}
+
+
+class APIPostTests(TestCase):
+
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.user = User.objects.create(username='bubbaray', password='dudley')
+
+    def test_valid_submission(self):
+        view = DataList.as_view()
+        request = self.factory.post(
+            '/pushed_data/',
+            json.dumps(VALID_POST),
+            content_type='application/json'
+        )
+        force_authenticate(request, user=self.user)
+        response = view(request)
+
+        self.assertEqual(response.status_code, 201)
+
+    def test_missing_doi(self):
+        view = DataList.as_view()
+        invalid_data = copy.copy(VALID_POST)
+        invalid_data.pop('doi')
+        request = self.factory.post(
+            '/pushed_data/',
+            json.dumps(invalid_data),
+            content_type='application/json'
+        )
+        force_authenticate(request, user=self.user)
+        response = view(request)
+        data = response.data
+        self.assertEqual(data['doi'], ['This field is required.'])
+
+    def test_invalid_doi(self):
+        view = DataList.as_view()
+        invalid_data = copy.copy(VALID_POST)
+        invalid_data['doi'] = 'thisistotallynotadoi'
+        request = self.factory.post(
+            '/pushed_data/',
+            json.dumps(invalid_data),
+            content_type='application/json'
+        )
+        force_authenticate(request, user=self.user)
+        response = view(request)
+        data = response.data
+        self.assertEqual(
+            data['non_field_errors'],
+            ['DOI does not resolve, please enter a valid DOI']
+        )
