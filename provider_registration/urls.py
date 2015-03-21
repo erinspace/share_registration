72c09ed12e13066f657b0431f83af93a50ced628
erinspace <erin.braswell@gmail.com>
2015-03-21 14:18:35 -0400

    Remove outdated urls and update new ones

--- provider_registration/urls.py
+++ provider_registration/urls.py
@@ -8,7 +8,6 @@ urlpatterns = patterns(
     url(r'^$', views.index, name='index'),
     url(r'^provider_detail/(?P<provider_long_name>.*)/$', views.detail, name='detail'),
     url(r'^register', views.register_provider, name='register'),
-    url(r'^pre_register', views.get_provider_info, name='pre_register'),
     url(r'^contact_information', views.get_contact_info, name='contact_information'),
     url(r'^metadata_information', views.get_metadata_info, name='metadata_information'),
 
