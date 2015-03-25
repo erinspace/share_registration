0fdd215926aae309001bbd6cbba7b856892cf9b2
erinspace <erin.braswell@gmail.com>
2015-03-25 10:53:07 -0400

    Add Int field for provider timeout requests

--- provider_registration/models.py
+++ provider_registration/models.py
@@ -8,23 +8,24 @@ class RegistrationInfo(models.Model):
     link = 'Edit'
 
     # Contact Information
-    contact_name = models.CharField(max_length=100)
     contact_email = models.EmailField()
+    contact_name = models.CharField(max_length=100)
 
     # Terms of Service and Metadata Permissions Questions
     meta_tos = models.BooleanField(default=False)
     meta_license = models.CharField(max_length=100)
     meta_privacy = models.BooleanField(default=False)
     meta_sharing_tos = models.BooleanField(default=False)
-    meta_license_extended = models.BooleanField(default=False)
     meta_future_license = models.BooleanField(default=False)
+    meta_license_extended = models.BooleanField(default=False)
 
     #Provider Information
     base_url = models.URLField()
     description = models.TextField()
     oai_provider = models.BooleanField(default=False)
-    provider_short_name = models.CharField(max_length=50, blank=True, default='')
     provider_long_name = models.CharField(max_length=100)
+    request_rate_limit = models.PositiveSmallIntegerField(default=0)
+    provider_short_name = models.CharField(max_length=50, blank=True, default='')
 
     # Harvester Information
     property_list = models.TextField()
