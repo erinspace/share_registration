6523bcf97dc0bcc6c9ca9475fe6fa9afce193b44
erinspace <erin.braswell@gmail.com>
2015-03-30 17:31:48 -0400

    Add migration for adding established field

---
+++ push_endpoint/migrations/0003_auto_20150330_1631.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('push_endpoint', '0002_auto_20150325_1632'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Provider',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('established', models.BooleanField(default=False)),
+                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AlterModelOptions(
+            name='pusheddata',
+            options={'verbose_name': 'Data Object'},
+        ),
+    ]
