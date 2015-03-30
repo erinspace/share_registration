03c5f92f28cfc6ff994b1b0cd7f045c01001e707
erinspace <erin.braswell@gmail.com>
2015-03-30 17:30:46 -0400

    Add Provider model and established field

--- push_endpoint/models.py
+++ push_endpoint/models.py
@@ -1,16 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class PushedData(models.Model):
     url = models.URLField()
     doi = models.TextField()
-    tags = models.TextField(blank=True)
     title = models.TextField()
     serviceID = models.TextField()
-    description = models.TextField(blank=True)
     contributors = models.TextField()
+    tags = models.TextField(blank=True)
+    description = models.TextField(blank=True)
     dateUpdated = models.DateField(auto_now_add=True)
     source = models.ForeignKey('auth.User', related_name='data')
 
     class Meta:
         verbose_name = 'Data Object'
+
+
+class Provider(models.Model):
+    user = models.OneToOneField(User)
+    established = models.BooleanField(default=False)
