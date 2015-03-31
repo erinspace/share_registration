2372aae6fe86fd3a64820179adda9df3715abe06
erinspace <erin.braswell@gmail.com>
2015-03-30 22:06:13 -0400

    Add established checkbox to user admin

--- push_endpoint/admin.py
+++ push_endpoint/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 from push_endpoint.models import PushedData
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+
+from push_endpoint.models import Provider
+
+
+class ProviderInline(admin.StackedInline):
+    model = Provider
+    can_delete = False
+    verbose_name = 'provider'
+
+
+class UserAdmin(UserAdmin):
+    inlines = (ProviderInline, )
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(PushedData)
