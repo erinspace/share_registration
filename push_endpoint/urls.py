23257242676bace0771234edaf43aa078fb66e0b
erinspace <erin.braswell@gmail.com>
2015-04-03 14:32:14 -0400

    Add url patterns to display API keys

--- push_endpoint/urls.py
+++ push_endpoint/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import url
-from rest_framework.urlpatterns import format_suffix_patterns
 from push_endpoint import views
-
+from django.conf.urls import url
+from rest_framework.authtoken import views as auth_views
+from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.routers import DefaultRouter, SimpleRouter, Route
 
 urlpatterns = [
@@ -9,7 +9,8 @@ urlpatterns = [
     url(r'^pushed_data/established$', views.EstablishedDataList.as_view()),
     url(r'^pushed_data/(?P<pk>[0-9]+)/$', views.DataDetail.as_view(), name='data-detail'),
     url(r'^users/$', views.UserList.as_view()),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='user-detail')
+    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='user-detail'),
+    url(r'^get-api-key', views.render_api_form, name='get-api-key')
 ]
 
 
@@ -35,3 +36,7 @@ router.register(r'pushed_data/established', views.DataList.as_view(), base_name=
 urlpatterns = format_suffix_patterns(urlpatterns)
 
 urlpatterns += router.urls
+
+urlpatterns += [
+    url(r'^api-token-auth/', auth_views.obtain_auth_token, name='api-token-auth')
+]
