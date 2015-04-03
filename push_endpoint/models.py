dea8f7554f80fa06588137f3e92f1c8a2abb61af
erinspace <erin.braswell@gmail.com>
2015-04-03 14:31:10 -0400

    Add code to auto-generate token for new user

--- push_endpoint/models.py
+++ push_endpoint/models.py
@@ -1,5 +1,9 @@
 from django.db import models
+from django.conf import settings
+from django.dispatch import receiver
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from rest_framework.authtoken.models import Token
 
 
 class Provider(models.Model):
@@ -30,3 +34,9 @@ class PushedData(models.Model):
 
     class Meta:
         verbose_name = 'Data Object'
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
