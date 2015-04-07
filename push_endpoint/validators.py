9e58815880a54e29f8744eb891a6848f536db27d
erinspace <erin.braswell@gmail.com>
2015-04-07 16:18:23 -0400

    Update json validator, add TODO to fix later

--- push_endpoint/validators.py
+++ push_endpoint/validators.py
@@ -1,4 +1,7 @@
 ## custom validators
+from __future__ import unicode_literals
+
+import json
 import jsonschema
 from rest_framework.exceptions import ParseError
 
@@ -7,9 +10,13 @@ from push_endpoint import schemas
 
 class JsonSchema(object):
     def __call__(self, value):
+        json_text = value.get('jsonData')
+        # TODO - figure out why this isn't a dict automatically - this is
+        # problematic with pushed data with single quotes in it!!
+        json_text = json_text.replace("u'", '"').replace("'", '"')
+        json_data = json.loads(json_text)
 
-        json = value.get('jsonData')
         try:
-            jsonschema.validate(json, schemas.share)
-        except ValueError as error:
+            jsonschema.validate(json_data, schemas.share)
+        except jsonschema.exceptions.ValidationError as error:
             raise ParseError(detail=error.message)
