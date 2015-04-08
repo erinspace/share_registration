ba25870068f83cc8589f97b8c918076414387500
erinspace <erin.braswell@gmail.com>
2015-04-08 18:35:03 -0400

    Change the main index page to be the push api endpoint

--- shareregistration/urls.py
+++ shareregistration/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.conf.urls import patterns, include, url
 
-from shareregistration import views
+# from shareregistration import views
+from push_endpoint import views as push_views
 
 urlpatterns = patterns(
     '',
-    url(r'^$', views.index, name='index'),
+    url(r'^$', push_views.DataList.as_view(), name='index'),
     url(r'^', include('push_endpoint.urls')),
     url(r'^share-admin', include(admin.site.urls)),
     url(r'^provider_registration', include('provider_registration.urls', namespace="provider_registration"))
