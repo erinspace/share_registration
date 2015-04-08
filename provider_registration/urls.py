c46ffab522dc5a74a50789555ab0ee93db8c150f
erinspace <erin.braswell@gmail.com>
2015-04-08 19:15:46 -0400

    Modify index to use derived urls, change provide reg rl patterns

--- provider_registration/urls.py
+++ provider_registration/urls.py
@@ -1,15 +1,16 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from provider_registration import views
 
-urlpatterns = patterns(
-    '',
-    url(r'^$', views.index, name='index'),
-    url(r'^register', views.register_provider, name='register'),
-    url(r'^contact_information', views.get_contact_info, name='contact_information'),
-    url(r'^provider_information', views.save_metadata_render_provider, name='provider_information'),
-    url(r'^simple_oai_registration', views.redirect_to_simple_oai, name='simple_oai_registration')
-)
+urlpatterns = [
+    url(r'^/', include([
+        url(r'^$', views.index, name='index'),
+        url(r'^register/$', views.register_provider, name='register'),
+        url(r'^contact_information/$', views.get_contact_info, name='contact_information'),
+        url(r'^provider_information/$', views.save_metadata_render_provider, name='provider_information'),
+        url(r'^simple_oai_registration/$', views.redirect_to_simple_oai, name='simple_oai_registration')
+    ]))
+]
 
 urlpatterns += staticfiles_urlpatterns()
