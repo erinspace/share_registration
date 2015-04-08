83b3c9199c9c58672c98a9b5ce2d9e249c02873f
erinspace <erin.braswell@gmail.com>
2015-04-08 19:26:44 -0400

    Update swagger api docs and make all urls base again

--- push_endpoint/urls.py
+++ push_endpoint/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import include, url
 
 
 urlpatterns = [
-    url(r'^api/', include([
+    url(r'', include([
         url(r'^$', views.DataList.as_view()),
         url(r'^established/$', views.EstablishedDataList.as_view()),
         url(r'^(?P<pk>[0-9]+)/$', views.DataDetail.as_view(), name='data-detail'),
