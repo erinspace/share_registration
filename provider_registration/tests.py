5fbe4dd6ad38ca6f3156dcc4c12ff3cae0411495
erinspace <erin.braswell@gmail.com>
2015-04-17 11:27:53 -0400

    add test

--- provider_registration/tests.py
+++ provider_registration/tests.py
@@ -296,6 +296,26 @@ class ViewTests(TestCase):
         title = form_element.getchildren()[0]
         self.assertEqual(title.text, 'Simple Provider Information')
 
+    def test_registration_complete(self):
+        RegistrationInfo(
+            provider_long_name='Stardust Weekly',
+            base_url='http://wwe.com',
+            property_list=['some', 'properties'],
+            approved_sets=['some', 'sets'],
+            registration_date=timezone.now()
+        ).save()
+        request = self.factory.get('/')
+        provider_long_name = 'The COSMIC KEEEEEY'
+        base_url = 'http://wwe.com'
+        rate_limit = ''
+        api_docs = ''
+        description = 'A thing!'
+        new_registration = RegistrationInfo.objects.last()
+        reg_id = new_registration.pk
+        success = views.save_other_provider(request, provider_long_name, base_url, reg_id, api_docs, rate_limit, description)
+        self.assertTrue(success)
+        self.assertFalse(new_registration.registration_complete)
+
 
 class ViewMethodTests(TestCase):
 
