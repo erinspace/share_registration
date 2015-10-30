49eb4999eab3df625d6d4017239d94e0d04eac73
erinspace <erin.braswell@gmail.com>
2015-10-30 16:32:31 -0400

    Add gather provider info url for redirect

--- push_endpoint/urls.py
+++ push_endpoint/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
         url(r'^sources/$', views.UserList.as_view()),
         url(r'^(?P<pk>[0-9]+)/$', views.DataDetail.as_view(), name='data-detail'),
         url(r'^get-api-key/$', views.render_api_form, name='get-api-key'),
-        url(r'^help/$', views.render_api_help, name='help')
+        url(r'^help/$', views.render_api_help, name='help'),
+        url(r'^provider_information/$', views.provider_information, name='provider_information'),
+        url(r'^gather_provider_information/$', views.gather_provider_information, name='gather_provider_information')
     ]))
 ]
