f582e563f3395d03ece5c82f85b1cc0f40c3d2be
erinspace <erin.braswell@gmail.com>
2015-11-06 16:50:27 -0500

    Remove shortname from forms

--- push_endpoint/forms.py
+++ push_endpoint/forms.py
@@ -1,8 +1,9 @@
 from django import forms
+
 from push_endpoint.models import Provider
 
 
 class ProviderForm(forms.ModelForm):
     class Meta:
         model = Provider
-        fields = ['shortname', 'longname', 'url', 'favicon_image']
+        fields = ['longname', 'url', 'favicon_image']
