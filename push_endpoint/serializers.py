b057129d9bb052f2c5b1927dcfc12f5e5c34455a
erinspace <erin.braswell@gmail.com>
2015-11-12 16:23:12 -0500

    Add new create method to add docID and also shareProperties to the jsonData object

--- push_endpoint/serializers.py
+++ push_endpoint/serializers.py
@@ -1,3 +1,5 @@
+import json
+
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from rest_framework_bulk import BulkSerializerMixin, BulkListSerializer
@@ -8,15 +10,29 @@ from push_endpoint.validators import JsonSchema
 
 class PushedDataSerializer(BulkSerializerMixin, serializers.HyperlinkedModelSerializer):
     source = serializers.ReadOnlyField(source='source.username')
+    docID = serializers.ReadOnlyField()
 
     class Meta:
         model = PushedData
-        fields = ('id', 'collectionDateTime', 'source', 'jsonData')
+        fields = ('id', 'collectionDateTime', 'docID', 'source', 'jsonData')
         list_serializer_class = BulkListSerializer
 
         validators = [
             JsonSchema()
         ]
+
+    def create(self, validated_data):
+        json_text = validated_data.get('jsonData').replace("u'", '"').replace("'", '"')
+        json_data = json.loads(json_text)
+
+        json_data['shareProperties'] = {
+            'source': validated_data['source'].username,
+            'docID': json_data['uris']['providerUris'][0]
+        }
+
+        validated_data['jsonData'] = json_data
+        validated_data['docID'] = json_data['uris']['providerUris'][0]
+        return PushedData.objects.create(**validated_data)
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
