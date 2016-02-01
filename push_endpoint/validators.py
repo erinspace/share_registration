f8435031b1849b58bd844d5ea9a99c39c31e06b9
erinspace <erin.braswell@gmail.com>
2016-02-01 11:17:28 -0500

    Remove weird json transform code in validator

--- push_endpoint/validators.py
+++ push_endpoint/validators.py
@@ -1,28 +1,16 @@
 ## custom validators
 from __future__ import unicode_literals
 
-import json
 import jsonschema
-from rest_framework.exceptions import ParseError, ValidationError
+from rest_framework.exceptions import ParseError
 
 from push_endpoint import schemas
 
 
 class JsonSchema(object):
     def __call__(self, value):
-        json_text = value.get('jsonData')
-        # TODO - figure out why this isn't a dict automatically - this is
-        # problematic with pushed data with single quotes in it!!
-        json_text = json_text.replace("u'", '"').replace("'", '"')
-        try:
-            json_data = json.loads(json_text)
-        except ValueError as e:
-            raise ValidationError(
-                'Error: {} Please make sure your jsonData field is valid JSON.'.format(e)
-            )
-
         try:
             format_checker = jsonschema.FormatChecker()
-            jsonschema.validate(json_data, schemas.share, format_checker=format_checker)
+            jsonschema.validate(value.get('jsonData', {}), schemas.share, format_checker=format_checker)
         except jsonschema.exceptions.ValidationError as error:
             raise ParseError(detail=error.message)
